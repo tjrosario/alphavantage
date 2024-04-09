b1221a8596ed6277ebb95d5bb133075ca44ae1bc
Tommy Rosario <tom.jason.rosario@gmail.com>
2024-04-09 17:05:44 -0400

    set max width on dropdown

--- components/chart/ChartControls.tsx
+++ components/chart/ChartControls.tsx
@@ -13,7 +13,7 @@ export default function ChartControls({ onSymbolChange }: ChartControlProps) {
     <div>
       <Field label="Stock:">
         <select
-          className="w-auto border border-solid border-gray-400 rounded-md p-1"
+          className="w-auto border border-solid border-gray-400 rounded-md p-1 max-w-xs md:max-w-none"
           onChange={(e) => onSymbolChange(e.target.value)}
         >
           <option value={''}>Select Stock...</option>
