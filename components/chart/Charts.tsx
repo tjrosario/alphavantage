cd640067671c9517849eb1099048dff3976d77b7
Tommy Rosario <tom.jason.rosario@gmail.com>
2024-04-10 01:00:00 -0400

    sort by fiscal date

--- components/chart/Charts.tsx
+++ components/chart/Charts.tsx
@@ -4,9 +4,21 @@ import { fetchIncomeStatements } from '@/utils/api/incomeStatement';
 import { fetchBalanceSheet } from '@/utils/api/balanceSheet';
 import { ChartContext } from '@/components/chart/ChartProvider';
 import { useContext } from 'react';
-import { BalanceSheetReport, IncomeStatementReport } from '@/types/report';
 import Loader from '@/components/loader/Loader';
 import Alert from '@/components/alert/Alert';
+import { BalanceSheetReport, IncomeStatementReport } from '@/types/report';
+
+function sortByFiscalDate(data: any) {
+  if (!data) {
+    return [];
+  }
+
+  return [...data].sort(
+    (a, b) =>
+      new Date(a.fiscalDateEnding).valueOf() -
+      new Date(b.fiscalDateEnding).valueOf()
+  );
+}
 
 export default function Charts() {
   const {
@@ -35,43 +47,53 @@ export default function Charts() {
     setLoading(false);
   }
 
+  // Sort by fiscal date
+  const incomeStatementDataSorted: IncomeStatementReport[] = sortByFiscalDate(
+    incomeStatementData?.quarterlyReports
+  );
+
+  const balanceSheetDataSorted: BalanceSheetReport[] = sortByFiscalDate(
+    balanceSheetData?.quarterlyReports
+  );
+
   // Build out data set for the chart
-  const data = incomeStatementData?.quarterlyReports
-    ? {
-        labels: incomeStatementData?.quarterlyReports?.map(
-          (report: IncomeStatementReport) => report.fiscalDateEnding
-        ),
-        datasets: [
-          {
-            label: 'Net Income',
-            data: incomeStatementData?.quarterlyReports?.map(
-              (report: IncomeStatementReport) => report.netIncome
-            ),
-            fill: false,
-            borderColor: '#43911a',
-            tension: 0.1,
-          },
-          {
-            label: 'Total Revenue',
-            data: incomeStatementData?.quarterlyReports?.map(
-              (report: IncomeStatementReport) => report.totalRevenue
-            ),
-            fill: false,
-            borderColor: '#d5ae26',
-            tension: 0.1,
-          },
-          {
-            label: 'Total Shareholder Equity',
-            data: balanceSheetData?.quarterlyReports?.map(
-              (report: BalanceSheetReport) => report.totalShareholderEquity
-            ),
-            fill: false,
-            borderColor: '#5a76f7',
-            tension: 0.1,
-          },
-        ],
-      }
-    : null;
+  const data =
+    incomeStatementDataSorted?.length > 0
+      ? {
+          labels: incomeStatementDataSorted.map(
+            (report: IncomeStatementReport) => report.fiscalDateEnding
+          ),
+          datasets: [
+            {
+              label: 'Net Income',
+              data: incomeStatementDataSorted.map(
+                (report: IncomeStatementReport) => report.netIncome
+              ),
+              fill: false,
+              borderColor: '#43911a',
+              tension: 0.1,
+            },
+            {
+              label: 'Total Revenue',
+              data: incomeStatementDataSorted.map(
+                (report: IncomeStatementReport) => report.totalRevenue
+              ),
+              fill: false,
+              borderColor: '#d5ae26',
+              tension: 0.1,
+            },
+            {
+              label: 'Total Shareholder Equity',
+              data: balanceSheetDataSorted.map(
+                (report: BalanceSheetReport) => report.totalShareholderEquity
+              ),
+              fill: false,
+              borderColor: '#5a76f7',
+              tension: 0.1,
+            },
+          ],
+        }
+      : null;
 
   return (
     <div>
